3951116bd9db6502a49580afcc76fbbc5a0ca28c
Rana Emad <rana3emad@gmail.com>
2019-12-16 09:42:56 +0200

    add fragment adapter

---
+++ app/src/main/java/com/example/android/tourguide/FragmentAdapter.java
@@ -0,0 +1,33 @@
+package com.example.android.tourguide;
+
+import androidx.fragment.app.Fragment;
+import androidx.fragment.app.FragmentManager;
+import androidx.fragment.app.FragmentPagerAdapter;
+
+public class FragmentAdapter extends FragmentPagerAdapter {
+    public FragmentAdapter(FragmentManager fm){
+        super(fm);
+    }
+
+    @Override
+    public Fragment getItem(int position) {
+        if (position == 0) {
+            return new RestaurantsFragment();
+        }
+//         else if (position == 1){
+//            return new TuesdayFragment();
+//        } else if (position == 2) {
+//            return new WednesdayFragment();
+//        } else if (position == 3) {
+//            return new ThursdayFragment();
+//        } else {
+//            return new FridayFragment();
+//        }
+        return new RestaurantsFragment();
+    }
+
+    @Override
+    public int getCount() {
+        return 1;
+    }
+}
